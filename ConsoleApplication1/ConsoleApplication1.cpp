e4994b0a64666a82da07364209196a9a8ca99265
Dmitriy <Dmitriy@DESKTOP-2LVQF8K>
2023-09-19 21:53:44 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std; 
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    float num1, num2, result;
+    cout << "Введите первое число: "; 
+    cin >> num1;
+
+    cout << "Введите второе число: "; 
+    cin >> num2;
+
+    char math;
+    cout << "Введите действие: [+] [-] [*] [/] "; 
+    cin >> math;
+
+    if (math == '+')
+        result = num1 + num2;
+    else if (math == '-')
+        result = num1 - num2;
+    else if (math == '*')
+        result = num1 * num2;
+    else if (math == '/')
+        result = num1 / num2;
+
+    cout << "Ответ: " << result; 
+}
+
